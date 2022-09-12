8c79f610818538bdc5c009c8d62a6140885ec87b
David Emaye <emayeodavid@gmail.com>
2022-09-13 00:04:30 +0100

    developed forgot-password page

---
+++ pages/auth/forgot-password/index.vue
@@ -0,0 +1,365 @@
+<template>
+  <div class="container">
+    <div class="inner">
+      <div class="lhs">
+        <div class="logo">
+          <img src="~assets/images/logo-white.png" alt="">
+        </div>
+        <div class="intro slide-in-from-left">
+          <p class="intro_header">
+            Customised Portal for Field Health Workers
+          </p>
+          <p class="intro_subheader">
+            Log and Monitor your daily test order on your customised dashboard
+          </p>
+        </div>
+        <div class="mobile inner_ come-up no_show">
+          <div class="back">
+            Back
+          </div>
+          <p class="title">
+            Forgot Password?
+          </p>
+          <p class="sub-title">
+            Select which contact details should we use to
+            reset your password
+          </p>
+          <div class="form">
+            <div class="contact_box">
+              <svg width="40" height="40" viewBox="0 0 56 56" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g clip-path="url(#clip0_13_54)">
+                  <path d="M35.0002 56H21.0002C14.5672 56 9.3335 50.7663 9.3335 44.3333V11.6667C9.3335 5.23367 14.5672 0 21.0002 0H35.0002C41.4332 0 46.6668 5.23367 46.6668 11.6667V44.3333C46.6668 50.7663 41.4332 56 35.0002 56ZM21.0002 4.66667C17.1408 4.66667 14.0002 7.80733 14.0002 11.6667V44.3333C14.0002 48.1927 17.1408 51.3333 21.0002 51.3333H35.0002C38.8595 51.3333 42.0002 48.1927 42.0002 44.3333V11.6667C42.0002 7.80733 38.8595 4.66667 35.0002 4.66667H21.0002ZM32.6668 44.3333C32.6668 43.0453 31.6215 42 30.3335 42H25.6668C24.3788 42 23.3335 43.0453 23.3335 44.3333C23.3335 45.6213 24.3788 46.6667 25.6668 46.6667H30.3335C31.6215 46.6667 32.6668 45.6213 32.6668 44.3333Z" fill="#00295D" />
+                </g>
+                <defs>
+                  <clipPath id="clip0_13_54">
+                    <rect width="56" height="56" fill="white" />
+                  </clipPath>
+                </defs>
+              </svg>
+              <div class="contact_text">
+                <p class="head">
+                  Via SMS
+                </p>
+                <p class="text">
+                  *** **** *** 1406
+                </p>
+              </div>
+            </div>
+            <div class="contact_box">
+              <svg width="32" height="32" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g clip-path="url(#clip0_13_93)">
+                  <path d="M31.6667 1.66675H8.33333C6.12401 1.66939 4.00593 2.54822 2.4437 4.11045C0.88147 5.67268 0.00264643 7.79076 0 10.0001L0 30.0001C0.00264643 32.2094 0.88147 34.3275 2.4437 35.8897C4.00593 37.4519 6.12401 38.3308 8.33333 38.3334H31.6667C33.876 38.3308 35.9941 37.4519 37.5563 35.8897C39.1185 34.3275 39.9974 32.2094 40 30.0001V10.0001C39.9974 7.79076 39.1185 5.67268 37.5563 4.11045C35.9941 2.54822 33.876 1.66939 31.6667 1.66675ZM8.33333 5.00008H31.6667C32.6646 5.00204 33.6392 5.30261 34.4649 5.86309C35.2906 6.42357 35.9298 7.21833 36.3 8.14508L23.5367 20.9101C22.5974 21.8456 21.3257 22.3709 20 22.3709C18.6743 22.3709 17.4026 21.8456 16.4633 20.9101L3.7 8.14508C4.07025 7.21833 4.70936 6.42357 5.53508 5.86309C6.3608 5.30261 7.33536 5.00204 8.33333 5.00008ZM31.6667 35.0001H8.33333C7.00725 35.0001 5.73548 34.4733 4.7978 33.5356C3.86012 32.5979 3.33333 31.3262 3.33333 30.0001V12.5001L14.1067 23.2667C15.671 24.8272 17.7904 25.7035 20 25.7035C22.2096 25.7035 24.329 24.8272 25.8933 23.2667L36.6667 12.5001V30.0001C36.6667 31.3262 36.1399 32.5979 35.2022 33.5356C34.2645 34.4733 32.9927 35.0001 31.6667 35.0001Z" fill="#00295D" />
+                </g>
+                <defs>
+                  <clipPath id="clip0_13_93">
+                    <rect width="40" height="40" fill="white" />
+                  </clipPath>
+                </defs>
+              </svg>
+              <div class="contact_text">
+                <p class="head">
+                  Via email
+                </p>
+                <p class="text">
+                  *** **** *** ke@gmail.com
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="rhs">
+        <div class="rhs_inner">
+          <div class="top_btn">
+            <button class="trans_btn" @click="$router.go(-1)">
+              Back
+            </button>
+          </div>
+          <div class="inner_">
+            <p class="title">
+              Forgot Password?
+            </p>
+            <p class="sub-title">
+              Select which contact details should we use to
+              reset your password
+            </p>
+            <div class="form">
+              <div class="contact_box">
+                <svg width="56" height="56" viewBox="0 0 56 56" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <g clip-path="url(#clip0_13_54)">
+                    <path d="M35.0002 56H21.0002C14.5672 56 9.3335 50.7663 9.3335 44.3333V11.6667C9.3335 5.23367 14.5672 0 21.0002 0H35.0002C41.4332 0 46.6668 5.23367 46.6668 11.6667V44.3333C46.6668 50.7663 41.4332 56 35.0002 56ZM21.0002 4.66667C17.1408 4.66667 14.0002 7.80733 14.0002 11.6667V44.3333C14.0002 48.1927 17.1408 51.3333 21.0002 51.3333H35.0002C38.8595 51.3333 42.0002 48.1927 42.0002 44.3333V11.6667C42.0002 7.80733 38.8595 4.66667 35.0002 4.66667H21.0002ZM32.6668 44.3333C32.6668 43.0453 31.6215 42 30.3335 42H25.6668C24.3788 42 23.3335 43.0453 23.3335 44.3333C23.3335 45.6213 24.3788 46.6667 25.6668 46.6667H30.3335C31.6215 46.6667 32.6668 45.6213 32.6668 44.3333Z" fill="#00295D" />
+                  </g>
+                  <defs>
+                    <clipPath id="clip0_13_54">
+                      <rect width="56" height="56" fill="white" />
+                    </clipPath>
+                  </defs>
+                </svg>
+                <div class="contact_text">
+                  <p class="head">
+                    Via SMS
+                  </p>
+                  <p class="text">
+                    *** **** *** 1406
+                  </p>
+                </div>
+              </div>
+              <div class="contact_box">
+                <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <g clip-path="url(#clip0_13_93)">
+                    <path d="M31.6667 1.66675H8.33333C6.12401 1.66939 4.00593 2.54822 2.4437 4.11045C0.88147 5.67268 0.00264643 7.79076 0 10.0001L0 30.0001C0.00264643 32.2094 0.88147 34.3275 2.4437 35.8897C4.00593 37.4519 6.12401 38.3308 8.33333 38.3334H31.6667C33.876 38.3308 35.9941 37.4519 37.5563 35.8897C39.1185 34.3275 39.9974 32.2094 40 30.0001V10.0001C39.9974 7.79076 39.1185 5.67268 37.5563 4.11045C35.9941 2.54822 33.876 1.66939 31.6667 1.66675ZM8.33333 5.00008H31.6667C32.6646 5.00204 33.6392 5.30261 34.4649 5.86309C35.2906 6.42357 35.9298 7.21833 36.3 8.14508L23.5367 20.9101C22.5974 21.8456 21.3257 22.3709 20 22.3709C18.6743 22.3709 17.4026 21.8456 16.4633 20.9101L3.7 8.14508C4.07025 7.21833 4.70936 6.42357 5.53508 5.86309C6.3608 5.30261 7.33536 5.00204 8.33333 5.00008ZM31.6667 35.0001H8.33333C7.00725 35.0001 5.73548 34.4733 4.7978 33.5356C3.86012 32.5979 3.33333 31.3262 3.33333 30.0001V12.5001L14.1067 23.2667C15.671 24.8272 17.7904 25.7035 20 25.7035C22.2096 25.7035 24.329 24.8272 25.8933 23.2667L36.6667 12.5001V30.0001C36.6667 31.3262 36.1399 32.5979 35.2022 33.5356C34.2645 34.4733 32.9927 35.0001 31.6667 35.0001Z" fill="#00295D" />
+                  </g>
+                  <defs>
+                    <clipPath id="clip0_13_93">
+                      <rect width="40" height="40" fill="white" />
+                    </clipPath>
+                  </defs>
+                </svg>
+                <div class="contact_text">
+                  <p class="head">
+                    Via email
+                  </p>
+                  <p class="text">
+                    *** **** *** ke@gmail.com
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <ModalsResetPassword v-if="resetPassword" @sentInst="sentInst" @close-modal="resetPassword = false" />
+    <ModalsLinkSent v-if="linkSent" @set-password="setPassword" />
+    <ModalsCreatePassword v-if="createPassword" />
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      checked: false,
+      error: false,
+      email: '',
+      password: '',
+      type: 'password',
+      resetPassword: false,
+      createPassword: false,
+      linkSent: false
+    }
+  },
+  methods: {
+    showHide () {
+      if (this.type === 'password') {
+        this.type = 'text'
+      } else if (this.type === 'text') {
+        this.type = 'password'
+      }
+    },
+    toogleCheck () {
+      this.checked = !this.checked
+    },
+    sentInst () {
+      this.resetPassword = false
+      this.linkSent = true
+    },
+    setPassword () {
+      this.linkSent = false
+      this.createPassword = true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.inner {
+  display: flex;
+  height: 100vh;
+}
+
+.rhs {
+  flex-basis: 42%;
+  margin-left: 58%;
+}
+
+.rhs_inner {
+  width: 60%;
+  margin: auto;
+  padding: 4vh 0;
+}
+
+.top_btn {
+  display: flex;
+  justify-content: flex-end;
+}
+
+.trans_btn {
+  width: 9rem;
+}
+
+.inner_ {
+  height: 100%;
+  padding-top: 10vh;
+}
+
+.logo {
+  width: 9vw;
+}
+
+.logo img {
+  width: 100%;
+}
+
+.lhs {
+  width: 58%;
+  background-image: url('assets/images/login-bg.jpg');
+  background-size: cover;
+  background-position-y: bottom;
+  height: 100vh;
+  /* margin-left: 35%; */
+  position: fixed;
+  padding: 6vh 7vw;
+}
+
+.intro {
+  height: 100%;
+  width: 65%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.intro_header {
+  color: #fff;
+  font-size: 35px;
+  font-weight: 600;
+}
+
+.intro_subheader {
+  color: rgba(255, 255, 255, 0.392);
+  font-size: 16px;
+  margin-top: 10px;
+}
+
+.title {
+  font-size: 20px;
+  font-weight: 700;
+  color: var(--primary-color);
+}
+
+.sub-title {
+  color: black;
+  margin-top: 10px;
+}
+
+.form {
+  margin-top: 3rem;
+}
+
+.contact_box {
+  display: flex;
+  align-items: center;
+  gap: 15px;
+  border-radius: 10px;
+  background-color: #E5EAEF;
+  padding: 0 20px;
+  height: 12vh;
+  margin-bottom: 4vh;
+  cursor: pointer;
+}
+
+.contact_text .head {
+  color: var(--primary-color);
+  font-weight: 700;
+  font-size: 15px;
+}
+
+.contact_text .text {
+  color: var(--primary-color);
+  margin-top: 5px;
+  font-size: 15px;
+}
+
+@media only screen and (max-width: 500px) {
+  .back {
+    color: #00285d71;
+    font-size: 13px;
+  }
+  .inner {
+    flex-direction: column;
+  }
+
+  .logo {
+    display: none;
+  }
+
+  .lhs {
+    background-position: center;
+    position: unset;
+    width: 100%;
+    padding: 0;
+  }
+
+  .intro {
+    display: block;
+    width: 100%;
+    height: fit-content;
+    padding: 20vh 7vw 7vh;
+  }
+
+  .inner_ {
+    height: fit-content;
+    background-color: #fff;
+    border-radius: 20px 20px 0px 0px;
+    padding: 20px 30px;
+  }
+
+  .no_show {
+    display: block;
+  }
+
+  .rhs {
+    display: none;
+    width: 100%;
+    margin: 0;
+    padding: 20px 30px;
+  }
+
+  .rhs_inner {
+    width: 100%;
+  }
+
+  .intro_header {
+    font-size: 20px;
+  }
+
+  .intro_subheader {
+    font-size: 14px;
+  }
+
+  .top_btn {
+    display: none;
+  }
+
+  .title {
+    font-size: 18px;
+    margin-top: 1.5rem;
+  }
+
+  .sub-title {
+    font-size: 13px;
+  }
+
+  .form {
+    margin-top: 8vh;
+  }
+  .contact_text .head {
+    font-size: 13px;
+  }
+  .contact_text .text {
+    font-size: 13px;
+  }
+}
+
+</style>
