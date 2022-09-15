d5bd17695bc8c06368b7b2d7125403d943091921
David Emaye <emayeodavid@gmail.com>
2022-09-15 07:51:34 +0100

    developed next of kin reg page

---
+++ components/Registration/NextOfKin.vue
@@ -0,0 +1,166 @@
+<template>
+  <div class="form_container come-up">
+    <div class="form_inner">
+      <p class="title">
+        Next of Kin
+      </p>
+      <p class="sub_title">
+        Who else can we reach incase of emergency?
+      </p>
+      <div class="form">
+        <div class="input-box">
+          <p class="label">
+            Next of Kin First Name
+          </p>
+          <div class="new_input">
+            <input v-model="nok_first_name" placeholder="Your Legal First Name" type="text">
+          </div>
+        </div>
+        <div class="input-box">
+          <p class="label">
+            Next of Kin Last Name
+          </p>
+          <div class="new_input">
+            <input v-model="nok_last_name" placeholder="Your Legal Surname" type="text">
+          </div>
+        </div>
+        <div class="input-box">
+          <p class="label">
+            Next of Kin Phone Number
+          </p>
+          <div class="new_input">
+            <input v-model="nok_phone" placeholder="e.g. 45" type="text">
+          </div>
+        </div>
+        <div class="input-box">
+          <p class="label">
+            Next of Kin Address
+          </p>
+          <div class="new_input">
+            <input v-model="nok_address" placeholder="House Nuumber, Street , Bus Stop City," type="text">
+          </div>
+        </div>
+        <div class="input-box">
+          <p class="label">
+            Relationship to Next of Kin
+          </p>
+          <div class="form-select">
+            <select v-model="nok_relationship" required>
+              <option value="">
+                Select...
+              </option>
+              <option value="Brother">
+                Brother
+              </option>
+              <option value="Sister">
+                Sister
+              </option>
+              <option value="Friend">
+                Friend
+              </option>
+              <option value="Father">
+                Father
+              </option>
+              <option value="Mother">
+                Mother
+              </option>
+              <option value="Others">
+                Others
+              </option>
+            </select>
+            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M7.24652 11.14L2.45052 5.658C1.88452 5.013 2.34452 4 3.20352 4H12.7955C12.9878 3.99984 13.176 4.05509 13.3376 4.15914C13.4993 4.26319 13.6275 4.41164 13.707 4.58669C13.7864 4.76175 13.8137 4.956 13.7856 5.14618C13.7575 5.33636 13.6752 5.51441 13.5485 5.659L8.75252 11.139C8.65866 11.2464 8.54291 11.3325 8.41303 11.3915C8.28316 11.4505 8.14216 11.481 7.99952 11.481C7.85688 11.481 7.71589 11.4505 7.58601 11.3915C7.45614 11.3325 7.34038 11.2464 7.24652 11.139V11.14Z" fill="black" />
+            </svg>
+          </div>
+        </div>
+        <div v-if="nok_relationship === 'Others'" class="input-box come-down">
+          <p class="label">
+            Others (Specify)
+          </p>
+          <div class="new_input">
+            <input v-model="nok_relationship_others" placeholder="Specify Next of Kin Relationship" type="text">
+          </div>
+        </div>
+        <div class="bottom_btn">
+          <button class="trans_btn" @click="$emit('back')">
+            Back
+          </button>
+          <button class="bg_btn" @click="$emit('proceed')">
+            Save and Proceed
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      nok_first_name: '',
+      nok_last_name: '',
+      nok_phone: '',
+      nok_address: '',
+      nok_relationship: '',
+      nok_relationship_others: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+.form_container {
+  padding: 2rem;
+}
+
+.title {
+  font-size: 20px;
+  font-weight: 700;
+  text-align: center;
+  color: var(--primary-color);
+}
+
+.sub_title {
+  color: black;
+  margin-top: 10px;
+  text-align: center;
+}
+
+.form {
+  margin-top: 3rem;
+}
+
+.input-box {
+  margin-bottom: 3vh;
+}
+
+.bottom_btn {
+  display: flex;
+  justify-content: space-between;
+  margin-top: 6vh;
+}
+
+.trans_btn {
+  width: 48%;
+}
+
+.bg_btn {
+  width: 48%;
+}
+
+@media only screen and (max-width: 500px) {
+  .form_container {
+    padding: 10px 0;
+  }
+
+  .title {
+    font-size: 18px;
+    margin-top: 1.5rem;
+  }
+
+  .sub_title {
+    font-size: 13px;
+  }
+}
+</style>
--- pages/auth/register/details.vue
+++ pages/auth/register/details.vue
@@ -6,12 +6,13 @@
       </div>
       <div class="progress_bars">
         <hr class="progress_1 active_bar">
-        <hr :class="`progress_1 ${bar_active_2 ? 'active_bar' : ''}`">
+        <hr :class="`progress_1 ${nextOfKin ? 'active_bar' : ''}`">
         <hr :class="`progress_1 ${bar_active_3 ? 'active_bar' : ''}`">
         <hr :class="`progress_1 ${bar_active_4 ? 'active_bar' : ''}`">
       </div>
       <div class="form_ctn">
-        <RegistrationPersonalInformation v-if="personalInformation" @back="back" @proceed="proceed" />
+        <RegistrationPersonalInformation v-if="personalInformation" @back="back" @proceed="personalInformation = false; nextOfKin = true" />
+        <RegistrationNextOfKin v-if="nextOfKin" @back="back" @proceed="proceed" />
       </div>
     </div>
   </div>
@@ -21,7 +22,8 @@
 export default {
   data () {
     return {
-      personalInformation: true,
+      personalInformation: false,
+      nextOfKin: true,
       bar_active_2: false,
       bar_active_3: false,
       bar_active_4: false
@@ -36,7 +38,7 @@ export default {
 }
 
 .inner {
-  width: 40%;
+  max-width: 40%;
   margin: auto;
 }
 
@@ -64,6 +66,23 @@ export default {
 
 .active_bar {
   border-top: 6px solid #0FB2F3;
+}
+
+.form_ctn {
+  height: 80vh;
+  margin: auto;
+  overflow: auto;
+}
+
+/* Hide scrollbar for Chrome, Safari and Opera */
+.form_ctn::-webkit-scrollbar {
+  display: none;
+}
+
+/* Hide scrollbar for IE, Edge and Firefox */
+.form_ctn {
+  -ms-overflow-style: none;  /* IE and Edge */
+  scrollbar-width: none;  /* Firefox */
 }
 
 @media only screen and (max-width: 500px) {
