cfa001634d9099589ae6eebc6611e8230209fdca
David Emaye <emayeodavid@gmail.com>
2022-10-07 10:41:31 +0100

    added mobile table loader

--- components/Tables/Loader.vue
+++ components/Tables/Loader.vue
@@ -1,26 +1,8 @@
 <template>
-  <div class="loading-container table-container">
-    <table>
-      <tr class="table-header">
-        <td class="date">
-          <div class="skeleton card" style="width: 100%; height: 40px" />
-        </td>
-        <td class="time">
-          <div class="skeleton card" style="width: 100%; height: 40px" />
-        </td>
-        <td class="time">
-          <div class="skeleton card" style="width: 100%; height: 40px" />
-        </td>
-        <td class="address">
-          <div class="skeleton card" style="width: 100%; height: 40px" />
-        </td>
-      </tr>
-      <tbody>
-        <tr
-          v-for="i in 3"
-          :key="i"
-          class="table-row"
-        >
+  <div>
+    <div class="loading-container table-container mobile_no_show">
+      <table>
+        <tr class="table-header">
           <td class="date">
             <div class="skeleton card" style="width: 100%; height: 40px" />
           </td>
@@ -34,24 +16,55 @@
             <div class="skeleton card" style="width: 100%; height: 40px" />
           </td>
         </tr>
-      </tbody>
-    </table>
-    <div class="table-footer">
-      <div class="page-select lhs">
-        <div class="skeleton card" style="width: 35px; height: 25px" />
-        <div class="skeleton card" style="width: 25px; height: 25px" />
-        <div class="skeleton card" style="width: 35px; height: 25px" />
+        <tbody>
+          <tr
+            v-for="i in 3"
+            :key="i"
+            class="table-row"
+          >
+            <td class="date">
+              <div class="skeleton card" style="width: 100%; height: 40px" />
+            </td>
+            <td class="time">
+              <div class="skeleton card" style="width: 100%; height: 40px" />
+            </td>
+            <td class="time">
+              <div class="skeleton card" style="width: 100%; height: 40px" />
+            </td>
+            <td class="address">
+              <div class="skeleton card" style="width: 100%; height: 40px" />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <div class="table-footer">
+        <div class="page-select lhs">
+          <div class="skeleton card" style="width: 35px; height: 25px" />
+          <div class="skeleton card" style="width: 25px; height: 25px" />
+          <div class="skeleton card" style="width: 35px; height: 25px" />
+        </div>
+        <div class="mhs">
+          <div class="skeleton card" style="width: 45px; height: 25px" />
+          <div class="skeleton card" style="width: 45px; height: 25px" />
+          <div class="skeleton card" style="width: 45px; height: 25px" />
+        </div>
+        <div class="rhs">
+          <div class="skeleton card" style="width: 50px; height: 25px" />
+          <div class="skeleton card" style="width: 25px; height: 25px" />
+          <div class="skeleton card" style="width: 25px; height: 25px" />
+          <div class="skeleton card" style="width: 50px; height: 25px" />
+        </div>
       </div>
-      <div class="mhs">
-        <div class="skeleton card" style="width: 45px; height: 25px" />
-        <div class="skeleton card" style="width: 45px; height: 25px" />
-        <div class="skeleton card" style="width: 45px; height: 25px" />
-      </div>
-      <div class="rhs">
-        <div class="skeleton card" style="width: 50px; height: 25px" />
-        <div class="skeleton card" style="width: 25px; height: 25px" />
-        <div class="skeleton card" style="width: 25px; height: 25px" />
-        <div class="skeleton card" style="width: 50px; height: 25px" />
+    </div>
+    <div class="no_show">
+      <div
+        v-for="i in 3"
+        :key="i"
+        class="mobile_loader"
+      >
+        <div class="text_card skeleton card" />
+        <div class="text_card skeleton card" />
+        <div class="text_card skeleton card" />
       </div>
     </div>
   </div>
@@ -137,5 +150,17 @@ tr.table-row {
   border-radius: 6px;
   padding: 7px 10px;
   font-weight: 500;
+}
+
+.mobile_loader {
+  /* border: 1px solid rgba(0, 0, 0, 0.111); */
+  border-radius: 5px;
+  padding: 3px 7px;
+  margin-bottom: 20px;
+}
+
+.text_card {
+  height: 25px;
+  margin: 4px 0;
 }
 </style>
