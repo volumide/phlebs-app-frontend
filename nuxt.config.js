ca5a90ce71d0879184a9a46b2e3a1869c11b3f85
David Emaye <emayeodavid@gmail.com>
2022-10-17 00:41:18 +0100

    added pwa

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,6 +33,7 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/pwa',
     '@nuxtjs/eslint-module'
   ],
 
